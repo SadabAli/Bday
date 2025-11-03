0a7213c1afa6bc2c29632f00c35e5c314b497ba9
SadabAli <alisadab404@gmail.com>
2025-11-03 11:33:10 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -178,11 +178,11 @@ $(document).ready(function () {
     function msgLoop(i) {
       $('p:nth-child(' + i + ')')
         .fadeOut('slow')
-        .delay(1000)
+        .delay(900)
         .promise()
         .done(function () {
           i = i + 1;
-          $('p:nth-child(' + i + ')').fadeIn('slow').delay(5500);
+          $('p:nth-child(' + i + ')').fadeIn('slow').delay(6900);
           if (i == 50) {
             $('p:nth-child(49)')
               .fadeOut('slow')
